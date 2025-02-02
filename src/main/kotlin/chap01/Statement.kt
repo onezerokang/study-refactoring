51a6ecc50bc9c329cf776ff10bc3e8ee892d7881
onezerokang <onezerokang@gmail.com>
2025-02-02 15:08:36 +0900

    chap01: volumeCredits 계산 코드를 독립적인 반복문으로 분리

--- src/main/kotlin/chap01/Statement.kt
+++ src/main/kotlin/chap01/Statement.kt
@@ -52,11 +52,14 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
     var result = "청구 내역 (고객명: ${invoice.customer})\n"
 
     for (perf: Invoice.Performance in invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf)
         // 청구 내역을 출력한다.
         result += " ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n"
         totalAmount += amountFor(perf)
     }
+    for (perf: Invoice.Performance in invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf)
+    }
+
     result += "총액: ${usd(totalAmount)}\n"
     result += "적립 포인트: ${volumeCredits}점\n"
     return result
