698fe496611de8c67248b0e1f50bc0f5e749e2be
onezerokang <onezerokang@gmail.com>
2025-02-03 08:29:29 +0900

    chap01: HTML 버전 Statement 메서드 추가

--- src/main/kotlin/chap01/Statement.kt
+++ src/main/kotlin/chap01/Statement.kt
@@ -77,11 +77,15 @@ fun statement(invoice: Invoice, plays: Map<String, Play>): String {
     return renderPlainText(createStatementData(invoice, plays))
 }
 
-fun renderPlainText(data: StatementData): String {
+fun htmlStatement(invoice: Invoice, plays: Map<String, Play>): String {
+    return renderHtml(createStatementData(invoice, plays))
+}
 
-    fun usd(amount: Int): String {
-        return NumberFormat.getCurrencyInstance(Locale.US).format(amount / 100)
-    }
+fun usd(amount: Int): String {
+    return NumberFormat.getCurrencyInstance(Locale.US).format(amount / 100)
+}
+
+fun renderPlainText(data: StatementData): String {
 
     var result = "청구 내역 (고객명: ${data.customer})\n"
 
@@ -92,5 +96,22 @@ fun renderPlainText(data: StatementData): String {
 
     result += "총액: ${usd(data.totalAmount)}\n"
     result += "적립 포인트: ${data.totalVolumeCredits}점\n"
+    return result
+}
+
+fun renderHtml(data: StatementData): String {
+
+    var result = "<h1>청구 내역 (고객명: ${data.customer})</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>"
+
+    for (perf: StatementData.EnrichedPerformance in data.performances) {
+        // 청구 내역을 출력한다.
+        result += "<tr><td>${perf.play.name}</td><td>${perf.audience}석</td>"
+        result += "<td>${usd(perf.amount)}</td></tr>\n"
+    }
+    result += "</table>\n"
+    result += "<p>총액: <em>${usd(data.totalAmount)}</em></p>\n"
+    result += "<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n"
     return result
 }
--- src/test/kotlin/chap01/StatementTest.kt
+++ src/test/kotlin/chap01/StatementTest.kt
@@ -33,4 +33,36 @@ class StatementTest {
         """.trimIndent() + '\n'
         )
     }
+
+    @Test
+    fun htmlStatement() {
+        val plays = mapOf(
+            "hamlet" to Play(name = "Hamlet", type = "tragedy"),
+            "as-like" to Play(name = "As You Like It", type = "comedy"),
+            "othello" to Play(name = "Othello", type = "tragedy"),
+        )
+        val invoice = Invoice(
+            customer = "BigCo",
+            performances = listOf(
+                Invoice.Performance("hamlet", 55),
+                Invoice.Performance("as-like", 35),
+                Invoice.Performance("othello", 40)
+            )
+        )
+
+        val result = htmlStatement(invoice, plays)
+
+        assertThat(result).isEqualTo(
+            """
+            <h1>청구 내역 (고객명: BigCo)</h1>
+            <table>
+            <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr><tr><td>Hamlet</td><td>55석</td><td>$650.00</td></tr>
+            <tr><td>As You Like It</td><td>35석</td><td>$580.00</td></tr>
+            <tr><td>Othello</td><td>40석</td><td>$500.00</td></tr>
+            </table>
+            <p>총액: <em>$1,730.00</em></p>
+            <p>적립 포인트: <em>47</em>점</p>
+        """.trimIndent() + '\n'
+        )
+    }
 }
