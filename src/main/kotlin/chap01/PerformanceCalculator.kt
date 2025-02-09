98d8ebdce0797e613ef051dce1343e3cab9f2236
Wonyoung Kang <onezerokang@gmail.com>
2025-02-09 12:00:05 +0900

    chap01: PerformanceCalculator를 추상 클래스로 변경

--- src/main/kotlin/chap01/PerformanceCalculator.kt
+++ src/main/kotlin/chap01/PerformanceCalculator.kt
@@ -2,14 +2,12 @@ package chap01
 
 import kotlin.math.max
 
-open class PerformanceCalculator(
+abstract class PerformanceCalculator(
     val performance: Invoice.Performance,
     val play: Play,
 ) {
 
-    open fun amount(): Int {
-        throw RuntimeException("서브클래스에서 처리하도록 설계되었습니다.")
-    }
+    abstract fun amount(): Int
 
     open fun volumeCredits(): Int {
         return max(performance.audience - 30, 0)
