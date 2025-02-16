6d17fb5caf2916cc57b99b2f6815109c1937cb5a
Wonyoung Kang <onezerokang@gmail.com>
2025-02-16 18:00:12 +0900

    chap04: 테스트 추가하기

--- src/test/kotlin/chap04/ProvinceTest.kt
+++ src/test/kotlin/chap04/ProvinceTest.kt
@@ -1,17 +1,30 @@
 package chap04
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 @DisplayName("province")
 class ProvinceTest {
+    private lateinit var asia: Province
+
+    @BeforeEach
+    fun setUp() {
+        asia = Province(sampleProvinceData())
+    }
 
     @DisplayName("shortfall")
     @Test
     fun shortfall() {
         val asia = Province(sampleProvinceData())
         assertThat(asia.shortfall()).isEqualTo(5)
+    }
+
+    @DisplayName("profit")
+    @Test
+    fun profit() {
+        assertThat(asia.profit()).isEqualTo(230)
     }
 
 }
